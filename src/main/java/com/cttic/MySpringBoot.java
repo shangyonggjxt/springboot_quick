2b97355357361389f80b5cc274f1ba9cfb580c27
shangyong <shangyong808@126.com>
2019-04-28 15:49:55 +0800

    test

---
+++ src/main/java/com/cttic/MySpringBoot.java
@@ -0,0 +1,11 @@
+package com.cttic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBoot.class);
+    }
+}
